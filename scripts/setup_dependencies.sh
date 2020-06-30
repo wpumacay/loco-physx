47a66a070861213d0d81a67f4a71a3b4f972e0d5
wpumacay <wpumacay@gmail.com>
2020-06-30 17:50:46 -0500

    [Maintenance]: updated build using main-PhysX
    Changes:
    
    * Updated the build steps to use the PhysX version from the main nvidia
      repository, instead of the previously used fork that added integration
      for cmake (was getting some issues related to not building the physx-
      extensions-library). The new method requires PhysX to be built and
      installed into the "ext/physx_build" directory at the root of the
      main project, and all libraries are added from there
    
    TODOs:
    
    * There's still much room for improvements, as the workflow is hard-
      coded to use the debug build of PhysX only (see the CMake files). In
      the next commits we might have to add functionality to handle building
      PhysX as an external project along the cmake workflow and use the
      appropriate variables to set the paths correctly for the right version

--- scripts/setup_dependencies.sh
+++ scripts/setup_dependencies.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 GIT_DEPS_REPO=(tiny_math tiny_utils pybind11 imgui spdlog tiny_renderer tysoc googletest PhysX)
-GIT_DEPS_USER=(wpumacay wpumacay RobotLocomotion wpumacay gabime wpumacay wpumacay google phcerdan)
-GIT_DEPS_BRANCH=(master master drake docking v1.x master master master cmake_for_easier_integration)
+GIT_DEPS_USER=(wpumacay wpumacay RobotLocomotion wpumacay gabime wpumacay wpumacay google NVIDIAGameWorks)
+GIT_DEPS_BRANCH=(master master drake docking v1.x master master master 4.1)
 GIT_DEPS_DEST=(ext/tiny_math ext/tiny_utils ext/pybind11 ext/imgui ext/spdlog ext/tiny_renderer core ext/googletest ext/physx)
 
 for i in {0..8}
